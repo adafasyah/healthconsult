692f25335a5efcc1b056242fbfd5259243a7c753
A D Afasyah <afasyah@netmonk.id>
2024-12-08 10:09:30 +0700

    Initial commit

---
+++ src/main/java/com/group3/healthconsult/HealthconsultApplication.java
@@ -0,0 +1,13 @@
+package com.group3.healthconsult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthconsultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthconsultApplication.class, args);
+	}
+
+}
