bef02198f99b026147ac7252b666a7d43d1fdb5f
adafasyah <141480323+adafasyah@users.noreply.github.com>
2024-12-15 19:09:02 +0700

    add migration (#11)

---
+++ src/main/java/com/group3/healthconsult/migrations/specialization.sql
@@ -0,0 +1,14 @@
+INSERT INTO specialization (name)
+VALUES
+    ('Mata'),
+    ('Gigi dan Mulut'),
+    ('THT'),
+    ('Jantung'),
+    ('Organ Dalam'),
+    ('Kulit dan Kelamin'),
+    ('Bedah'),
+    ('Anak'),
+    ('Kandungan'),
+    ('Psikiater'),
+    ('Fisioterapi'),
+    ('Dokter Umum');
